239c9e88f5b5a4cbf2463f52d4d043c76a79eae9
SpEcTrEeLeEe <daniilmanuylov239@gmail.com>
2023-05-14 19:53:22 +0300

    Continue of work with Angular

--- ProjectOnANgular/burgers/src/app/app.component.ts
+++ ProjectOnANgular/burgers/src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'burgers';
+export class AppComponent
+{
+  form = this.fb.group({
+      order: ["", Validators.required],
+      name: ["", Validators.required],
+      phone: ["", Validators.required]
+    })
+  constructor(private fb: FormBuilder) {
+  }
+  scrollTo(target: HTMLElement)
+  {
+    target.scrollIntoView({behavior:"smooth"})
+  }
+
+  confirmOrder()
+  {
+    if (this.form.valid)
+    {
+      alert("Спасибо за заказ! Мы скоро свяжемся с вами!")
+    }
+  }
 }
